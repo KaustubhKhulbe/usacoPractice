5deeb95f46fe4695999792bdfedc6af3fe71f8f1
kaustubh <kaustubh.khulbe@gmail.com>
2020-08-28 10:25:47 -0700

    Worked on Triangles

--- src/main/java/USACO_Training_Gateway/Bronze_Triangles_2020/triangles.java
+++ src/main/java/USACO_Training_Gateway/Bronze_Triangles_2020/triangles.java
@@ -4,7 +4,7 @@ package USACO_Training_Gateway.Bronze_Triangles_2020;
 public class triangles {
 
     private static int n;
-    private static int[][] points = { {0,0}, {0,1}, {0,2}, {0,3}   };;
+    private static int[][] points = { {0,0}, {0,1}, {0,2}, {0,3}, {0,4}, {0,5}   };;
 
     public static void main(String[] args){
 
@@ -13,18 +13,57 @@ public class triangles {
         //points = new int[n][2];
        // points = { {0,0}, {0,1}, {0,2}, {0,3}   };
 
+       int[][][] answer = calculate(points);
+       for(int i = 0; i < answer.length;i++){
+           for(int j = 0; j < 3; j++){
+               for(int k = 0; k < 2; k++){
+                   System.out.print(   "(" + answer[i][j][k] + ")"   );
+               }
 
+           }
+           System.out.println();
+       }
     }
 
-    public static int calculate(int[][] points){
+    public static int[][][] calculate(int[][] points){
+
+        int numCount = factorial(points.length)/(factorial(points.length-3) * factorial(3));
+        int[][][] answers = new int[numCount][3][2];
+        int c = 0;
+
         for(int i=0;i<points.length;i++){
-            for(int j=1;j<points.length-1;j++){
-                for(int k=2;k<points.length-2;k++){
-                    if(k == j || k == i) break;
-                    System.out.println(points[i][0] + ","+ points[j][0] + "," + points[k][0]);
-                    //calculateArea(points[i], points[j], points[k])
+            System.out.println(i);
+            for(int j=i+1;j<points.length;j++){
+                System.out.println(j);
+                for(int k=j+1;k<points.length;k++){
+                    System.out.println(i + " " + j + " " + k + " ");
+                    answers[c][0] = points[i];
+                    answers[c][1] = points[j];
+                    answers[c][2] = points[k];
+                    c++;
                 }
-                if(j == i) break;
+            }
+        }
+        System.out.println("done");
+        return answers;
+    }
+
+    public static int factorial(int n){
+        int sum = 1;
+        for(int i = 1; i<n+1; i++){
+            sum *= i;
+        }
+        return sum;
+    }
+
+    public static int finMaxArea(int[][][] answers){
+        int max = 0;
+        int temp = 0;
+        for(int i = 0; i < answers.length; i++){
+            for (int j = 0; j< 3; j++){
+                for(int k = 0; k < 2; k++){
+
+                }
             }
         }
         return 0;
