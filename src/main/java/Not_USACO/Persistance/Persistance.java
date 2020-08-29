8c8dc5e299b0458deb2a36375b50d130c92944fd
kaustubh <kaustubh.khulbe@gmail.com>
2020-08-29 16:27:05 -0700

    Worked on persistance.java

---
+++ src/main/java/Not_USACO/Persistance/Persistance.java
@@ -0,0 +1,55 @@
+package Not_USACO.Persistance;
+
+public class Persistance {
+
+    public static void main(String[] args){
+
+        System.out.println(additivePersistence(138));
+
+    }
+
+    public static int additivePersistence(int n) {
+
+        String nToString = Integer.toString(n);
+        int length = nToString.length();
+        int finalSum = 10;
+        int counter = 0;
+        while (finalSum >= 10){
+            int sum = 0;
+            for(int i = 0; i < length; i++){
+                sum += n%10;
+                n -= n%10;
+                n/= 10;
+
+            }
+            finalSum = sum;
+            counter++;
+        }
+
+
+        return counter;
+
+    }
+
+    public static int multiplicativePersistence(int n) {
+        String nToString = Integer.toString(n);
+        int length = nToString.length();
+        int finalSum = 10;
+        int counter = 0;
+        while (finalSum >= 10){
+            int sum = 0;
+            for(int i = 0; i < length; i++){
+                sum += n%10;
+                n -= n%10;
+                n/= 10;
+
+            }
+            finalSum = sum;
+            counter++;
+        }
+
+
+        return counter;
+    }
+
+}
