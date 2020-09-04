7282dc4afcfd157dcb6314022b39360ec7714e49
kaustubh <kaustubh.khulbe@gmail.com>
2020-09-04 11:25:25 -0700

    Friday the 13th

--- src/main/java/USACO_Training_Gateway/Friday_The_Thirteenth/friday.java
+++ src/main/java/USACO_Training_Gateway/Friday_The_Thirteenth/friday.java
@@ -1,81 +1,58 @@
 package USACO_Training_Gateway.Friday_The_Thirteenth;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-
-import static java.lang.Integer.parseInt;
 
 public class friday {
 
-        public static File file;
-        public static BufferedReader bf;
-        public static String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-        public static int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        //public static int[] leapMonths = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        public static int[] dayCount = {0,0,0,0,0,0,0};
+        private static int N;
+        private static int[] monthDayCounts = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        private static int[] weekFridayCounts = {0,0,0,0,0,0,0};
 
-        public static void main(String[] args) throws Exception {
+        public static void main(String[] args){
+            N = 20;
+            iterateYears();
 
-            file = new File("friday.in");
-            bf = new BufferedReader(new FileReader(file));
-
-            //Get years from text file
-            int years = parseInt(bf.readLine());
-            years = 20; //TODO: remove later
-
-            countYears(years);
-            //System.out.println(countYears(years));
-            System.out.println(dayCount);
-            for(int i:dayCount) System.out.println(i);
-
+            for(int i: weekFridayCounts) System.out.println(i);
 
         }
 
-        private static void countYears(int years) {
+    private static void iterateYears() {
 
-            int dc = 0, answerCount = 0;
+            int totalDays = 0;
 
-            for(int i = 0; i < years; i++){
-                if (isLeapYear(i)){
+            for(int i = 0; i < N; i++){
 
-                    months[1] = 29;
-                    for(int j= 0; j < months.length; j++){
-                        for(int k = 0; k < months[j]; k++){
-                            dc++;
-                            if(k == 12) {
-                                dayCount[dc % 7]++;
-                                System.out.println(dc % 7);
-                            }
 
-                        }
-                    }
-                    months[1] = 28;
-                }
-                for(int j= 0; j < months.length; j++){
+                if(ifIsLeapYear(1900 + N)) monthDayCounts[1] = 29;
 
-                    for(int k = 0; k < months[j]; k++){
-                        dc++;
+                for(int j = 0; j < 12; j++){
+                    for(int k = 0; k < monthDayCounts[j]; k++){
+                        totalDays++;
                         if(k == 12){
-                            dayCount[dc%7] ++;
+                            weekFridayCounts[(totalDays-1) % 7]++;
                         }
-
                     }
                 }
+                monthDayCounts[1] = 28;
 
             }
-
-
-        }
-
-        private static boolean isLeapYear(int i) {
-            if ((i%4 == 0 && i % 100 != 0) || i % 400 == 0){
-                return true;
-            }
-            return false;
-        }
-
 
     }
+
+    private static boolean ifIsLeapYear(int n) {
+
+            if((n % 4 == 0 && n % 100 != 0) || n%400 == 0) return true;
+            return false;
+
+    }
+
+    /*for(int j = 0; j < monthDayCounts[i]; j++){
+                    totalDays++;
+                    if(j == 12){
+                        weekFridayCounts[totalDays % 7]++;
+                    }
+                }*/
+
+
+}
 
 
