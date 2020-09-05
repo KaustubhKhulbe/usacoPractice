74b739ac2354a0da50d3f59b423311bf2652cc63
kaustubh <kaustubh.khulbe@gmail.com>
2020-09-05 00:34:30 -0700

    FINISHED FRIDAY!!!

--- src/main/java/USACO_Training_Gateway/Friday_The_Thirteenth/friday.java
+++ src/main/java/USACO_Training_Gateway/Friday_The_Thirteenth/friday.java
@@ -1,58 +1,68 @@
 package USACO_Training_Gateway.Friday_The_Thirteenth;
-
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class friday {
 
-        private static int N;
-        private static int[] monthDayCounts = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        private static int[] weekFridayCounts = {0,0,0,0,0,0,0};
+    /* Use the slash-star style comments or the system won't see your
+   identification information */
+    /*
+    ID: kaustub11
+    LANG: JAVA
+    TASK: friday
+*/
 
-        public static void main(String[] args){
-            N = 20;
-            iterateYears();
-
-            for(int i: weekFridayCounts) System.out.println(i);
-
-        }
-
-    private static void iterateYears() {
-
-            int totalDays = 0;
-
-            for(int i = 0; i < N; i++){
+    private static int N;
+    private static int[] monthDayCounts = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static int[] weekFridayCounts = {0,0,0,0,0,0,0};
 
 
-                if(ifIsLeapYear(1900 + N)) monthDayCounts[1] = 29;
+    public static void main(String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader("/Users/kkhulbe/usacoPractice/src/main/java/USACO_Training_Gateway/Friday_The_Thirteenth/friday.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("/Users/kkhulbe/usacoPractice/src/main/java/USACO_Training_Gateway/Friday_The_Thirteenth/friday.out")));
+        StringTokenizer st = new StringTokenizer(f.readLine());
 
-                for(int j = 0; j < 12; j++){
-                    for(int k = 0; k < monthDayCounts[j]; k++){
-                        totalDays++;
-                        if(k == 12){
-                            weekFridayCounts[(totalDays-1) % 7]++;
-                        }
+        N = Integer.parseInt(st.nextToken());
+
+        int totalDays = 0;
+
+        for(int i = 0; i < N; i++){
+
+            if(isLeapYear(i)) monthDayCounts[1] = 29;
+
+            for(int j = 0; j < monthDayCounts.length;j++){
+                for(int k = 0; k < monthDayCounts[j]; k++){
+
+                    totalDays++;
+                    if(k == 12) {
+                        weekFridayCounts[(totalDays)%7]+=1;
                     }
-                }
-                monthDayCounts[1] = 28;
 
+
+                }
             }
 
+            monthDayCounts[1] = 28;
+        }
+
+
+        out.print(weekFridayCounts[6] + " ");
+        out.print(weekFridayCounts[0] + " ");
+        out.print(weekFridayCounts[1] + " ");
+        out.print(weekFridayCounts[2] + " ");
+        out.print(weekFridayCounts[3] + " ");
+        out.print(weekFridayCounts[4] + " ");
+        out.print(weekFridayCounts[5] + "\n");
+        out.close();
+
+
     }
 
-    private static boolean ifIsLeapYear(int n) {
-
-            if((n % 4 == 0 && n % 100 != 0) || n%400 == 0) return true;
-            return false;
-
+    private static boolean isLeapYear(int n) {
+        n += 1900;
+        if((n%4 == 0 && n%100 != 0) || n%400 == 0) return true;
+        return false;
     }
-
-    /*for(int j = 0; j < monthDayCounts[i]; j++){
-                    totalDays++;
-                    if(j == 12){
-                        weekFridayCounts[totalDays % 7]++;
-                    }
-                }*/
-
 
 }
-
 
