1131285e31f64f91204ce35a64bec1627d49232a
kaustubh <kaustubh.khulbe@gmail.com>
2020-09-08 15:12:09 -0700

    worked on beads

--- src/main/java/USACO_Training_Gateway/BrokenNecklace/beads.java
+++ src/main/java/USACO_Training_Gateway/BrokenNecklace/beads.java
@@ -13,82 +13,89 @@ public class beads {
     TASK: beads
 */
     //random test / cleanup
-    private static String beads = "";
+    private static String beads = "wwwbbrwrbrbrrbrbrwrwwrbwrwrrb";
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader f = new BufferedReader(new FileReader("/Users/kkhulbe/usacoPractice/src/main/java/USACO_Training_Gateway/BrokenNecklace/beads.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("/Users/kkhulbe/usacoPractice/src/main/java/USACO_Training_Gateway/BrokenNecklace/beads.out")));
-        if(f == null) return;
         StringTokenizer st = new StringTokenizer(f.readLine());
+        int crap = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(f.readLine());
+
         beads = st.nextToken();
+        System.out.println(beads);
+
+        int maxBeads = getBeadCount();
+        out.println(maxBeads);
+        System.out.println(maxBeads);
+
         out.close();
-        beads = "wwwbbrwrbrbrrbrbrwrwwrbwrwrrb";
-        System.out.println(maxPossibleBeads());
-
-
     }
 
-    public static int maxPossibleBeads(){
+    private static int getBeadCount() {
         int max = 0;
-
         for(int i = 0; i < beads.length(); i++){
             String[] beadHalves = split(i);
-            String first = beadHalves[0];
-            String second = beadHalves[1];
+            String first = beadHalves[0], second = beadHalves[1];
+            String brokenBeads = reverse(first) + reverse(second);
 
-            StringBuilder reversedFirst = new StringBuilder(first);
-            reversedFirst = reversedFirst.reverse();
-            first = reversedFirst.toString();
+            int firstHalfCount = countBeads((brokenBeads)), secondHalfCount = countBeads(reverse(brokenBeads));
 
-            StringBuilder reversedSecond = new StringBuilder(second);
-            reversedSecond = reversedSecond.reverse();
-            second = reversedSecond.toString();
-
-            String brokenBeads = first + second;
-
-            int leftBeads = countBeads(brokenBeads);
-            
-            StringBuilder brokenReversed = new StringBuilder(brokenBeads);
-            brokenBeads = brokenReversed.reverse().toString();
-            int rightBeads = countBeads(brokenBeads);
-
-            int total = rightBeads + leftBeads;
-            if (total > max) max = total;
+            int currentMax = firstHalfCount + secondHalfCount;
+            if(allAreSame(brokenBeads)){
+                currentMax = brokenBeads.length();
+            }
+            if(currentMax > max) max = currentMax;
         }
-
         return max;
-
     }
 
-    private static int countBeads(String brokenBeads) {
-
-        int beadCount = 0;
-        char firstBead = 'w';
+    private static boolean allAreSame(String brokenBeads) {
+        char firstChar = 'w';
         for(int i = 0; i < brokenBeads.length(); i++){
             if(brokenBeads.charAt(i) != 'w') {
-                firstBead = brokenBeads.charAt(i);
+                firstChar = brokenBeads.charAt(i);
                 break;
             }
         }
 
         for(int i = 0; i < brokenBeads.length(); i++){
-            if(brokenBeads.charAt(i) == firstBead || brokenBeads.charAt(i) == 'w') beadCount++;
+            if(brokenBeads.charAt(i) != firstChar && brokenBeads.charAt(i) != 'w') return false;
+        }
+        return true;
+    }
+
+    private static int countBeads(String brokenBeads) {
+
+        char firstBeadColor = 'w';
+        int beadCount = 0;
+        for(int i = 0; i < brokenBeads.length(); i++){
+            if(brokenBeads.charAt(i) != 'w') {
+                firstBeadColor = brokenBeads.charAt(i);
+                break;
+            }
+        }
+        
+        for(int i = 0; i < brokenBeads.length(); i++){
+            if(brokenBeads.charAt(i) == firstBeadColor || brokenBeads.charAt(i) == 'w') beadCount++;
             else break;
         }
         return beadCount;
-    }
-
-    private static String[] split(int i) {
-
-        String first = beads.substring(0, i);
-        String second = beads.substring(i);
-
-        return new String[] {first, second};
 
     }
 
+    private static String[] split(int i){
+        String firstHalf = beads.substring(0, i);
+        String secondHalf = beads.substring(i);
+        return new String[] {firstHalf, secondHalf};
+    }
 
+    private static String reverse(String str){
+        StringBuilder builder = new StringBuilder(str);
+        return builder.reverse().toString();
+
+    }
 
 
 }
