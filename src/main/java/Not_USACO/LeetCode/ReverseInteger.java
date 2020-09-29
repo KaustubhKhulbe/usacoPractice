e2d77b32660d17f448ab11437bf984d87e332181
kaustubh <kaustubh.khulbe@gmail.com>
2020-09-29 11:08:23 -0700

    ReverseInteger

---
+++ src/main/java/Not_USACO/LeetCode/ReverseInteger.java
@@ -0,0 +1,22 @@
+package Not_USACO.LeetCode;
+
+class ReverseInteger {
+
+    public static void main(String[] args){
+        System.out.println(reverse(1234));
+        System.out.println(reverse(21));
+        System.out.println(reverse(203));
+    }
+
+    public static int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
