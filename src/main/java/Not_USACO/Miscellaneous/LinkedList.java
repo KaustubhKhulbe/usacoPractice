b3c75ba9c395899d538e6b2fb1e4bc277890a79f
kaustubh <kaustubh.khulbe@gmail.com>
2020-10-24 18:35:27 -0700

    Linked list basic implementation <No generic types yet>

---
+++ src/main/java/Not_USACO/Miscellaneous/LinkedList.java
@@ -0,0 +1,40 @@
+package Not_USACO.Miscellaneous;
+
+public class LinkedList {
+
+	private Link first;
+	
+	public LinkedList() {
+		
+	}
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
+	
+	public void insertAtBegining(int id, double value) {
+		Link link = new Link(id, value);
+		link.next = first;
+		first = link;
+	}
+	
+	public int size() {
+		int count = 0;
+		Link current = first;
+		while(current != null) {
+			count++;
+			current = current.next;
+		}
+		return count;
+	}
+	
+	public void printAll() {
+		Link current = first;
+		
+		while(current != null) {
+			System.out.println(current + " ");
+			current = current.next;
+		}
+	}
+
+}
