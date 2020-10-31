11558753cf9135412f7a1cb37ab5db2aeb6c9864
kaustubh <kaustubh.khulbe@gmail.com>
2020-10-31 11:51:29 -0700

    is unique finished

--- src/main/java/CrackingTheCodingInterview/ArraysAndStrings/isUnique.java
+++ src/main/java/CrackingTheCodingInterview/ArraysAndStrings/isUnique.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 public class isUnique {
 
     public static void main(String[] args){
-        System.out.println(isUnique("abcdefghijklmonpqrstuvwxyz", 0));
+        System.out.println(isUnique("abcdefghijklmnopqrstuvwxyz", 0.0));
     }
 
 
@@ -31,6 +31,21 @@ public class isUnique {
             for(int j = i+1; j < s1.length(); j++){ //O(n)
                 if(s1.charAt(j) == s1.charAt(i)) return false; //O(1)
             }
+        }
+        return true;
+    }
+
+    /*
+    Time complexity : O(n) -> Can also be max of 128,
+    so can be O(min(c, 128)), where c is length of string
+    Space complexity : O(1)
+     */
+    public static boolean isUnique(String s1, double notUseful){
+        if(s1.length() > 128) return false;
+        boolean[] chars = new boolean[128];
+        for(int i = 0; i < s1.length(); i++){
+            if(!chars[(int) s1.charAt(i)]) chars[(int) s1.charAt(i)] = true;
+            else return false;
         }
         return true;
     }
